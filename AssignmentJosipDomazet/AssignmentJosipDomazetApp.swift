1dd52b5e719386361d9a1d63dbc7242e5de0e774
user <user@iMac-von-user-4.local>
2023-09-22 08:35:43 +0200

    init

---
+++ AssignmentJosipDomazet/AssignmentJosipDomazetApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentJosipDomazetApp.swift
+//  AssignmentJosipDomazet
+//
+//  Created by user on 22.09.23.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentJosipDomazetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
