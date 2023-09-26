939f1ee1f8d604fc13eb243f393ba5666e8d898d
user <user@iMac-von-user-4.local>
2023-09-26 11:37:28 +0200

    refined the detailpage

--- AssignmentJosipDomazet/DetailView.swift
+++ AssignmentJosipDomazet/DetailView.swift
@@ -2,31 +2,79 @@ import SwiftUI
 
 struct ItemDetailsView: View {
     var item: Item
-
+    @AppStorage("showImages") private var showImages = true
+    
     var body: some View {
-        VStack(alignment: .leading, spacing: 16) { // Adjust alignment and spacing
+        ScrollView {
+            VStack(alignment: .center, spacing: 16) {
+                if let imageUrl = item.imageUrl, showImages {
+                    AsyncImage(url: URL(string: imageUrl)) { phase in
+                        switch phase {
+                        case .empty:
+                            ProgressView()
+                                .frame(width: 200, height: 200)
+                                .background(Color.gray)
+                                .cornerRadius(10)
+                        case .success(let image):
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 300)
+                                .overlay(
+                                    Text(item.title)
+                                        .font(.headline)
+                                        .foregroundColor(.white)
+                                        .padding(8)
+                                        .frame(maxWidth: .infinity, minHeight: 55)
+                                        .background(Color.black.opacity(0.6))
+                                    ,
+                                    alignment: .top
+                                )
+                                .cornerRadius(10)
+                            
+                        case .failure:
+                            Image(systemName: "photo")
+                                .frame(width: 200, height: 200)
+                                .background(Color.gray)
+                                .cornerRadius(10)
+                        @unknown default:
+                            EmptyView()
+                        }
+                    }
+                } else {
+                    
+                    Text(item.title)
+                        .font(.headline)
+                    
+                    Text("Images not displayed")
+                        .font(.caption2)
+                        .foregroundColor(.red)
+                }
+                
+            }
+            
             Text("ID: \(item.id)")
-                .font(.headline)
+                .font(.system(size: 9))
+                .foregroundColor(.gray)
             
-            Text("Title: \(item.title)")
-                .font(.title)
-            
-            Text("Description:")
-                .font(.subheadline)
             Text(item.description)
-                .font(.body)
+                .font(.body).padding(8)
             
             if let imageUrl = item.imageUrl {
-                Text("Image URL:")
-                    .font(.subheadline)
-                Text(imageUrl)
-                    .font(.body)
-            } else {
-                Text("Image URL: N/A")
-                    .font(.subheadline)
+                Button(action: {
+                    if let url = URL(string: imageUrl) {
+                        UIApplication.shared.open(url)
+                    }
+                }) {
+                    Text("Open Image URL")
+                        .font(.headline)
+                        .foregroundColor(.blue)
+                }.padding(16)
             }
         }
-        .padding(16) // Add padding to the entire content
+        .padding(16)
         .navigationBarTitle("Item Details")
+        
     }
 }
+
